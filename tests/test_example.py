031719902a6b90280718a4fb23253f6f0cb0806f
简朴 <60537770+Blissful-Jasper@users.noreply.github.com>
2025-04-03 09:25:40 +0800

    Update and rename example.py to tests/test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,28 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on %(date)s
-
-@author: %(username)s
-
-@email : xianpuji@hhu.edu.cn
-"""
-
 
 import matplotlib.pyplot as plt
 import numpy as np
 from Easyxp import simple_quiver_legend
 
-# 创建示例数据
+
 x = np.linspace(0, 2*np.pi, 10)
 y = np.sin(x)
 u = np.cos(x)
 v = np.sin(x)
 
-# 创建图形
+
 fig, ax = plt.subplots(dpi=200)
 q = ax.quiver(x, y, u, v)
 
-# 添加图例
+
 simple_quiver_legend(
     ax=ax,
     quiver=q,
