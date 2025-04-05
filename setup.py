bf02583674bf791a13e130e4928efa32591bf06c
简朴 <60537770+Blissful-Jasper@users.noreply.github.com>
2025-04-05 13:20:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = open(readme_path).read() if os.path.exists(readme_path) else 
 
 setup(
     name="easyxp",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "matplotlib>=3.4",
